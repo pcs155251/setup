27c5c2cae018405e4e19e1080e8ba3488ca61c70
pcs155251 <pcs155251@gmail.com>
2022-09-18 20:24:30 +0800

    add zsh_setup.bash

---
+++ zsh_setup.bash
@@ -0,0 +1,17 @@
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# install zsh-syntax-hilighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# install fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+# change plugins
+
+# source
+source ~/.zshrc
